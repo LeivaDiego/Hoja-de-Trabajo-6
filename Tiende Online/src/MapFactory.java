09f58e5db2074e40a78b63ec73b7fac8134031a5
LeivaDiego <lei21752@uvg.edu.gt>
2023-03-06 19:09:23 -0600

    Version 3.0 Map Factory
    Implementacion del patron de diseño factory para la instancia de maps

--- Tiende Online/src/MapFactory.java
+++ Tiende Online/src/MapFactory.java
@@ -1,2 +1,28 @@
-public class MapFactory {
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Patron de diseño Factory para la instancia del map
+ */
+public class MapFactory<K,V> {
+
+    /**
+     * Instancia del map usando el patron de diseño factory
+     * @param option opcion de mapa seleccionada por el usuario
+     * @return el map
+     */
+    public Map<K,V> createMap(int option){
+        Map<K,V> map = null;
+        switch (option){
+            case 1:
+                map = new HashMap<K,V>();
+            case 2:
+                map = new TreeMap<K,V>();
+            case 3:
+                map = new LinkedHashMap<K,V>();
+        }
+        return map;
+    }
 }
